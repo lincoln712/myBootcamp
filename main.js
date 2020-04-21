6ef90aee547559b6981a4b46aea710b956347e21
Howard Lincoln <lincolntheproud@gmail.com>
2020-04-21 12:43:22 -0300

    it has been added some changes on dev-news section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*home settings*/
 let arrowOpenMenu = document.querySelector(".open-menu");
 let arrowCloseMenu = document.querySelector(".close-menu");
 let sideMenu = document.querySelector("aside");
@@ -29,3 +30,39 @@ arrowCloseMenu.addEventListener('click',function(){
 		sideMenu.style.height = "0vh";
 	},300);
 });
+/*end home settings*/
+
+/*dev-news settings*/
+let dev = document.querySelectorAll(".dev");
+let ps = document.querySelectorAll("#dev-news p");
+let readMore = document.querySelectorAll("#dev-news .read-more");
+let content = document.querySelectorAll("#dev-news .content");
+//let readLess = document.querySelectorAll("#dev-news .read-less");
+//let readLess = document.createElement("a");
+
+readMore.forEach(function(button){
+	button.addEventListener('click',function(e){
+		/*remove styles from the previous element sibling*/
+		button.previousElementSibling.style.all = "unset";
+		button.style.display = "none";
+		
+		let readLess = document.createElement("a");
+		readLess.textContent = "Read Less";
+		readLess.className = "read-less";
+		buttonCreated = true;
+		/*add read less button before the read more on the clicked card because you need to grab the previous element sibling and it must be the paragraph*/
+		dev[e.target.id.replace("b","")-1].insertBefore(readLess,button);
+		
+		/*read less functionality*/
+		readLess.addEventListener('click',function(){
+			/*later on look it up about this css "all" property*/
+			readLess.previousElementSibling.className = "#dev-news .dev p";
+		
+			//console.log(readLess.previousElementSibling);
+			readLess.style.display = "none";
+			button.style.display = "inline";
+		});
+	});
+});
+
+/*end dev-news settings*/
